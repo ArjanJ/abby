a4434aa42ad2c8509e3aae0c9d651025a6c9c434
Arjan Jassal <arjan@ettrics.com>
2016-02-11 23:49:41 -0800

    create sticky header

---
+++ docs/docs.js
@@ -0,0 +1,38 @@
+(function(window, document) {
+
+  var header = document.querySelector('.docs-header');
+  var spacer = document.querySelector('.docs-header-spacer');
+
+  var originalOffsetTop = undefined;
+
+  document.addEventListener('DOMContentLoaded', handleLoad);
+
+  window.addEventListener('scroll', handleScroll);
+  window.addEventListener('resize', handleLoad);
+
+  function handleScroll() {
+
+    var scrolled = window.pageYOffset;
+
+    if (scrolled < originalOffsetTop && header.classList.contains('docs-header--fixed')) {
+      header.classList.remove('docs-header--fixed');
+      spacer.classList.remove('docs-header-spacer--active');
+    }
+
+    if (scrolled >= (originalOffsetTop) && !(header.classList.contains('docs-header--fixed'))) {
+      header.classList.add('docs-header--fixed');
+      spacer.classList.add('docs-header-spacer--active');
+    }
+  }
+
+  function handleLoad() {
+    var headerOffset = header.getBoundingClientRect();
+    originalOffsetTop = headerOffset.top;
+
+    if (window.pageYOffset < originalOffsetTop && header.classList.contains('docs-header--fixed')) {
+      header.classList.remove('docs-header--fixed');
+      spacer.classList.remove('docs-header-spacer--active');
+    }
+  }
+
+}(window, document));
